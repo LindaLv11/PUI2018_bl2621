f89d19afa4f3a4715dc84f81d6f15113d0d38fac
LindaLv11 <759804696@qq.com>
2018-09-25 21:47:13 -0400

    Add files via upload

---
+++ HW2_bl2621/show_bus_locations_bl2621.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 18:37:13 2018
+
+@author: 吕柏蓉
+"""
+import pandas as pd
+import pylab as pl
+import os
+import json
+from collections import defaultdict
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+import requests
+import sys
+
+#%pylab inline
+pl.rc('font', size=15)
+if not len(sys.argv) == 3:
+    print('Invalid number of arguments')
+    sys.exit()
+    
+
+url = 'http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=' + sys.argv[1]
+print(url)
+response = urllib.urlopen(url)
+data = response.read().decode("utf-8")
+data = json.loads(data)
+print(type(data))
+searchdic = defaultdict(list)
+name = sys.argv[2]
+count = 0
+answer = []
+length = len(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
+for i in range(length):
+    try:
+        newkey = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][i]['MonitoredVehicleJourney']['PublishedLineName']
+    except KeyError:
+        continue
+    if newkey == name:
+        count += 1
+        answer.append(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][i]['MonitoredVehicleJourney']['VehicleLocation'])
+print("Bus Line:"+ str(name))
+print("Number of Active Buses : " + str(count))
+if count == 0:
+    print("There is no data for this bus line currently, please try another bus line")
+for i in range(count):
+    la = str(answer[i]['Latitude'])
+    lo = str(answer[i]['Longitude'])
+    print("Bus "+ str(i) + " is at latitude "+ la + ' and longitude ' + lo)
+
